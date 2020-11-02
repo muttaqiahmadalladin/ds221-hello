5ebf001647c82d7dff620e25a3abd637dbe9babd
Muttaqi Ahmad Alladin <47301319+muttaqiahmadalladin@users.noreply.github.com>
2020-11-02 20:47:40 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+void printHello(){
+   cout << "Hello World \n";
+}
+
+int main() {
+    int i=0;
+    for (int i=0;i<10;i++){
+      printHello();
+    }
+ return 0;
+}
